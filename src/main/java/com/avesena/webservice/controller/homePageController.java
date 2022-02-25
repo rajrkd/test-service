bf8ee8f1ed145910973733d1be1aac33b2efe357
smit.jadhav07@gmail.com <smit.jadhav07@gmail.com>
2022-02-24 22:51:34 -0600

    Thymeleaf configuration added.

--- src/main/java/com/avesena/webservice/controller/homePageController.java
+++ src/main/java/com/avesena/webservice/controller/homePageController.java
@@ -5,22 +5,14 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
 import com.avesena.webservice.service.homeService;
 
-@RestController
+@Controller
 public class homePageController {
 
     @Value("spring.application.name")
     String appName;
-
-    public String getAppName() {
-        return appName;
-    }
-
-    public void setAppName(String appName) {
-        this.appName = appName;
-    }
 
     @GetMapping("/")
     public String homePage(Model model){
