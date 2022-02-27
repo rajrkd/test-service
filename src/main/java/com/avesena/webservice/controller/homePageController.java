ea27aaffde1f444c03900d6006fee307071032ef
smit.jadhav07@gmail.com <smit.jadhav07@gmail.com>
2022-02-26 18:34:09 -0600

    Database and Hikari configuration commit.

--- src/main/java/com/avesena/webservice/controller/homePageController.java
+++ src/main/java/com/avesena/webservice/controller/homePageController.java
@@ -1,12 +1,9 @@
 package com.avesena.webservice.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.stereotype.Controller;
-import com.avesena.webservice.service.homeService;
 
 @Controller
 public class homePageController {
