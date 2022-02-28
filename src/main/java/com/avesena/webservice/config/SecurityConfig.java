8f11e975c4e53e72020b6e7c32be20e76ad50364
smit.jadhav07@gmail.com <smit.jadhav07@gmail.com>
2022-02-27 19:54:54 -0600

    Spring Security code commit.

---
+++ src/main/java/com/avesena/webservice/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.avesena.webservice.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http
+                .authorizeRequests()
+                .antMatchers("/index").permitAll() // This will be your home screen URL
+                .antMatchers("/css/**").permitAll()
+                .antMatchers("/img/**").permitAll()
+                .antMatchers("/js/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/logon")
+                .permitAll();
+    }
+}
--- src/main/java/com/avesena/webservice/controller/homePageController.java
+++ src/main/java/com/avesena/webservice/controller/homePageController.java
@@ -1,6 +1,5 @@
 package com.avesena.webservice.controller;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.stereotype.Controller;
@@ -8,10 +7,7 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class homePageController {
 
-    @Value("spring.application.name")
-    String appName;
-
-    @GetMapping("/")
+    @GetMapping("/index")
     public String homePage(Model model){
         return "index";
     }
